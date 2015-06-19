62f72e0e179e4fc85eb5776ed7862c5e2614d0d5
cgopalakrishnan <me@chandra.bio>
2015-06-19 13:35:41 -0400

    Adding solution to file
    I have updated this file with the program I wrote

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x, n) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() matrix(x, n, n)
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  
 }
 
-
-## Write a short comment describing this function
-
+##cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
+##testing the function
+cacheSolve(makeCacheMatrix(c(1,2,3,4), 2))#Using cachesolve to invert a 2 by 2 matrix
+
